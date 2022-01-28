0349de1d6009945aa1a28c794c07a3fdc4e10547
Josh Tillinghast <jwtill@icloud.com>
2022-01-28 12:14:20 -0800

    WIP, add beginning of UI, troubleshooting getting the value from checkboxes. Break for lunch

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,10 @@ let superDuper = new Pizza(["peppers", "CHEESE", "ears", "tanKs", "Butter"], "l"
 Pizza.prototype.cost = function() {
   let toppingsCheck = this.toppings.map(toppings => toppings.toLowerCase());
   let pizzaCost = toppingsCheck.length * 2;  //calculate topping cost
-  
-  if (toppingsCheck.includes("cheese")) { //If cheese is included in the topping list, remove one item from the list
+
+  if (toppingsCheck.includes("cheese")) { //If cheese is included in the topping list, remove 2 from total
       pizzaCost = pizzaCost - 2; 
   }
-
   if (this.size === "s") {
     pizzaCost = pizzaCost + 15;
   }
@@ -28,16 +27,21 @@ Pizza.prototype.cost = function() {
     console.log("error");
     return 0;
   }
-  
   console.log("Your pizza costs: $", pizzaCost)
   return pizzaCost;
-
-
 }
-  // if (this.toppings === "cheese") {
-  //   this.toppings = this.toppings.unShift;
 
-  //   console.log("length afterward:", toppings.length);
-  // }
-  
+//UI Logic
+
+$(document).ready(function() {
+  $("form#order").submit (function() {
+    event.preventDefault();
+    let toppings = $("input[type='checkbox']").val();
+    console.log("Toppings are: ", toppings);
+    let size = $("#size").val();
+    console.log("Size is: ", size);
+    let newOrder = new Pizza(toppings, size);
+  });
+
+});
 
