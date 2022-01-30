6e74b1dddd5a52ccd000f55cb2c26924216170d4
Josh Tillinghast <jwtill@icloud.com>
2022-01-29 16:34:26 -0800

    Add styling to form and total msg

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
     let newOrder = new Pizza(toppingsTotal, size);
     let numToppings = newOrder.toppings.length;
     $("#total-msg").show();
-
+    
     if (numToppings <= 1) {
       $("#pizza-toppings").append(newOrder.toppings); 
     }
