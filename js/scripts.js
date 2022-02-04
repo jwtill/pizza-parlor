e42c1a3d3528d239342082f541b4be04072559c4
Josh Tillinghast <jwtill@icloud.com>
2022-02-03 21:13:50 -0800

    Tweak pizza message to display proper english

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,23 @@
 // Business Logic
-function Pizza(toppings, size) {//Pizza constructor with toppings, size, and cost properties
+function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
   this.cost = 0;
 }
 
-//cost method that determines cost 
 Pizza.prototype.pieTotal = function() {
   let toppingsCheck = this.toppings.map(toppings => toppings.toLowerCase());
   let pizzaCost = toppingsCheck.length * 2;  
-
   if (toppingsCheck.includes("cheese")) { 
       pizzaCost = pizzaCost - 2; 
   }
   if (this.size === "small") {
     pizzaCost = pizzaCost + 15;
-  }
-  else if(this.size === "medium") {
+  } else if(this.size === "medium") {
     pizzaCost = pizzaCost + 19;
-  }
-  else if(this.size === "large") {
+  } else if(this.size === "large") {
     pizzaCost = pizzaCost + 23;
-  }
-  else {
+  } else {
     return 0;
   }
   return pizzaCost;
@@ -35,39 +30,30 @@ $(document).ready(function() {
     event.preventDefault();
     $("#pizza-size").html("");
     $("#pizza-toppings").html("");
-
-    //get toppings from check boxes
     let toppingsTotal = [];
     $("input:checkbox").each(function() { 
       if ($(this).is(":checked")){
         toppingsTotal.push(this.value);
       }
     });
-    //get size from text input
     const size = $("#size").val();
-
-    //create new pizza with those inputs
     const newOrder = new Pizza(toppingsTotal, size);
     newOrder.cost = newOrder.pieTotal();
-
-    //create variable to hold the number of toppings
     const numToppings = newOrder.toppings.length;
     $("#total-msg").show();
-    
-    //Syntax for building sentence in total message
-    if (numToppings <= 1) {
+    if (numToppings === 0) {
       $("#pizza-toppings").append(newOrder.toppings); 
-    }
-    else {
+    } else if (numToppings === 1) {
+      $("#pizza-toppings").append("with " + newOrder.toppings); 
+    } else {
+      $("#pizza-toppings").append("with "); 
       for (let i = 0; i < numToppings; i++) { 
         if (i === numToppings - 1 ) { 
           $("#pizza-toppings").append(newOrder.toppings[i]); 
-        }
-        else if (i === numToppings - 2) { 
+        } else if (i === numToppings - 2) { 
           $("#pizza-toppings").append(newOrder.toppings[i] + " and "); 
-        }
-        else {
-          $("#pizza-toppings").append(newOrder.toppings[i] + " , "); 
+        } else {
+          $("#pizza-toppings").append(newOrder.toppings[i] + ", "); 
         }
       }
     }
